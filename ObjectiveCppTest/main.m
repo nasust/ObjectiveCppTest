f6e65f85e5b1e14fccba1b61576e2f9181032c1a
nasust <nasust@gmail.com>
2013-12-23 13:19:19 +0900

    Initial Commit

---
+++ ObjectiveCppTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ObjectiveCppTest
+//
+//  Created by 森 秀樹 on 2013/12/23.
+//  Copyright (c) 2013年 nasust. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
