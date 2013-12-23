591c868f958605ed19d1bd56357e280b5bb391c1
nasust <nasust@gmail.com>
2013-12-23 15:13:39 +0900

    commit project

--- ObjectiveCppTest/Class/TestClass.h
+++ ObjectiveCppTest/Class/TestClass.h
@@ -18,8 +18,19 @@ public:
         cout << m_name << "::Constructor: " << this << endl;
     }
 
+    TestClass(const TestClass & obj){
+        this->m_name = obj.m_name;
+        cout << m_name << "::CopyConstructor: " << this << endl;
+    }
+
     virtual ~TestClass() {
         cout << m_name << "::Destructor: " << this << endl;
+    }
+
+    TestClass & operator=( const TestClass & obj ){
+        this->m_name = obj.m_name;
+        cout << m_name << "::operator=: " << this << endl;
+        return *this;
     }
 
     void hello() const {
